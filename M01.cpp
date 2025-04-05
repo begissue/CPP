c16da9976fc3cec2a846b47e1978e01d21612dfc
begissue <doorbekovabegimaj@gmail.com>
2025-04-05 18:02:22 +0600

    Add files via upload

---
+++ M01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i{1}; i < 10; i++) {
+        for (int k(1); k < 10; k++) {
+            cout << i*k << "\t";;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
